c54e4c85e152643fcbbb2653afd77332322d6638
Yong Li <mail@liyong.cn>
2013-11-25 20:41:03 +0000

    Change the default value to list all sessions not only the pre-selected sessions.

--- application/controllers/signup4global.php
+++ application/controllers/signup4global.php
@@ -134,7 +134,7 @@ class SignUp4Global extends CI_Controller
 			$data['showchinaapproved'] = $this->input->post('showchinaapproved');
 		} else {
 			// defaut only list approved sessions
-			$data['showchinaapproved'] = TRUE;
+			$data['showchinaapproved'] = FALSE;
 		}
 
 		if ($data['showchinaapproved']) {
